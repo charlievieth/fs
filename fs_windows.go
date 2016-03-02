d1624adb6210fab2d75f29969f2fdb7beea9792b
Anthony Emengo <aemengo@pivotal.io>
2016-03-02 18:07:13 -0500

    Remove unused windows path functions
    Signed-off-by: Charlie Vieth <cviethjr@pivotal.io>

--- fs_windows.go
+++ fs_windows.go
@@ -10,31 +10,12 @@ import (
 // Max is 255 but use 245 to be safe.
 const winMaxLength = 245
 
-func longPath(p string) (string, error) {
-	path, err := absPath(p)
-	if err == nil && len(path) >= winMaxLength {
-		path = `\\?\` + path
-	}
-	return path, err
-}
-
 func absPath(path string) (string, error) {
 	if filepath.IsAbs(path) {
 		return filepath.Clean(path), nil
 	}
 	wd, err := os.Getwd()
 	return filepath.Join(wd, path), err
-}
-
-func osPath(path string) (string, error) {
-	p, err := absPath(path)
-	if err != nil {
-		return "", err
-	}
-	if len(p) >= winMaxLength {
-		p = `\\?\` + p
-	}
-	return p, nil
 }
 
 func Path(path string) (string, error) {
